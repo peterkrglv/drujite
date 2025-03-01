28e7e71bbdac535a5acb6271d063826726b77226
peterkrglv <peterkrglv@gmail.com>
2025-03-01 21:20:21 +0300

    setting up navigation for login

--- app/src/main/java/com/example/drujite/ui/signup/SignupState.kt
+++ app/src/main/java/com/example/drujite/ui/signup/SignupState.kt
@@ -6,15 +6,22 @@ sealed class SignupState {
     data class Main(
         val phone: String = "",
         val password: String = "",
+        val passwordRepeated: String = "",
         val gender: String = "",
         val isError: Boolean = false
     )
 }
 
 sealed class SignupEvent {
-
+    data class PhoneChanged(val phone: String) : SignupEvent()
+    data class PasswordChanged(val password: String) : SignupEvent()
+    data class PasswordRepeatedChanged(val passwordRepeated: String) : SignupEvent()
+    data class GenderChanged(val gender: String) : SignupEvent()
+    data object ProceedClicked : SignupEvent()
+    data object LoginClicked : SignupEvent()
 }
 
 sealed class SignupAction {
-
+    data object NavigateToLogin: SignupAction()
+    data object NavigateToSessionSelection: SignupAction()
 }
--- app/src/main/java/com/example/drujite/ui/signup/SignupViewModel.kt
+++ app/src/main/java/com/example/drujite/ui/signup/SignupViewModel.kt
@@ -2,5 +2,7 @@ package com.example.drujite.ui.signup
 
 import androidx.lifecycle.ViewModel
 
-class SignupViewModel: ViewModel() {
+class SignupViewModel: ViewModel(
+
+) {
 }
